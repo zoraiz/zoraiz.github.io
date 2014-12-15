546f4bb06a8fd8c41810761f5b7024db6e5f20c8
zoraiz <zoraiz@outlook.com>
2014-12-15 05:03:34 +0500

    Webflow Site
    Version 0.1

--- Index_files/main-b5943a91276e162cc7401acffa5ae349.js
+++
@@ -1,7 +0,0 @@
-(function() {
-var e, t;
-e = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i), t = $(window).width() <= 727 || $(window).height() < 400, 
-window.runAfterDomBinding.add("persona", function() {
-return Bobcat.TH.applyTouchNav(), 0 !== $(".side-header").length && $(".side-header").is(":visible") ? void 0 :$(".offset-three").removeClass("offset-three").addClass("offset-two");
-});
-}).call(this);
